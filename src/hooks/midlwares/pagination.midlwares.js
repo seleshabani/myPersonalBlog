cebc44fb067977937ae14cc19dbc783e88a9b203
seleshabani <seleshabani4@gmail.com>
2021-04-13 23:54:01 +0100

    pagination'

---
+++ src/hooks/midlwares/pagination.midlwares.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+
+const paginate = (model, search = false,byCat=false) => {
+    return async (req, res, next) => {
+        if (req.query.page) {
+            const page = parseInt(req.query.page);
+            const limit = parseInt(req.query.limit);
+            const startIdx = (page - 1) * limit;
+            const endIdx = page * limit;
+            const results = {};
+            
+            
+            try {
+                if (search) {
+                    results.results = await model.find({ name: { $regex: new RegExp(req.params.q) } })
+                        .limit(limit).skip(startIdx).exec();
+                    results.nbr = await model.find({ name: { $regex: new RegExp(req.params.q) } }).countDocuments();
+                  
+                }else if(byCat){
+                    results.results = await model.find({categorieId:req.query.catId}).limit(limit).skip(startIdx).exec();
+                    results.nbr = await model.find({categorieId:req.query.catId}).countDocuments()
+                } else {
+                    results.results = await model.find().limit(limit).skip(startIdx).exec();
+                    results.nbr = await model.find().countDocuments()
+                }
+                if (endIdx < results.nbr) {
+                    results.next = {
+                        page: page + 1,
+                        limit: limit
+                    }
+                }
+    
+                if (startIdx > 0) {
+                    results.previous = {
+                        page: page - 1,
+                        limit: limit
+                    }
+                }
+                
+                res.paginatedResults = results;
+                next();
+            } catch (error) {
+                console.log(error)
+                res.append(process.env.cors_header, process.env.authorized_cors_url).status(500).json('erreur serveur pendant la pagination');
+            }
+        } else {
+            next()
+        }
+    }
+}
+module.exports = { paginate }
--- src/routes/public/post.js
+++ src/routes/public/post.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const { localsDataMidlwares } = require('../../hooks/midlwares/locals.midlwares');
+const { paginate } = require('../../hooks/midlwares/pagination.midlwares');
 const postRouter = express.Router();
 const namedRouter = require('../../hooks/namedRouter');
+const { categorieModel } = require('../../model/categories');
 const { postModel } = require('../../model/posts');
 
 namedRouter.extendExpress(postRouter);
 postRouter.use(localsDataMidlwares)
+
+
 
 postRouter.get('/:id','post-single',async (req,res)=>{
     try {
@@ -26,4 +30,19 @@ postRouter.get('/:id','post-single',async (req,res)=>{
     }
 })
 
+postRouter.use(paginate(postModel,false,true));
+postRouter.get('/categories/views','post-byCat',async (req,res)=>{
+    try {
+        let id = req.query.catId;
+        // let posts = await postModel.find({categorieId:id});
+        let categorie = await categorieModel.findById(id);
+        res.locals.page = `categorie : ${categorie.name}`
+       // console.log(res.paginatedResults)
+        res.render('posts/by-categories',{posts:res.paginatedResults,categorie:categorie});
+    } catch (error) {
+        console.log(error)
+        res.status(500);
+        res.render('error/server');
+    }
+})
 module.exports = postRouter;
